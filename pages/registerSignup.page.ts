4302b32aa5569d65444cb2c05a66628a6d0a6543
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 12:48:09 +0200

    Update registerSignup.page.ts

--- pages/registerSignup.page.ts
+++ pages/registerSignup.page.ts
@@ -1,10 +1,15 @@
 import { Locator, Page, expect } from "@playwright/test";
+import { accountInformationFormUserData, registerSignupFormUserData } from "../data/registeredUserData.data";
 
 export class RegisterSignupPage {
   signupForm: Locator;
   signupNameInput: Locator;
   signupEmailInput: Locator;
   signupButton: Locator;
+  loginAccountHeader: Locator;
+  loginEmailInput: Locator;
+  loginPasswordInput: Locator;
+  loginButton: Locator;
 
   async verificationOfRegisterSignupPageLoadingAndFillingForm() {
     await expect(this.signupForm).toContainText("New User Signup!");
@@ -13,10 +18,21 @@ export class RegisterSignupPage {
     await this.signupButton.click();
   }
 
+  async loginWithCreatedUserData() {
+    await expect(this.loginAccountHeader).toContainText("Login to your account");
+    await this.loginEmailInput.fill(registerSignupFormUserData.email);
+    await this.loginPasswordInput.fill(accountInformationFormUserData.password);
+    await this.loginButton.click();
+  }
+
   constructor(private page: Page) {
     this.signupForm = page.locator(".signup-form");
     this.signupNameInput = page.locator("input[data-qa='signup-name']");
     this.signupEmailInput = page.locator("input[data-qa='signup-email']");
     this.signupButton = page.locator("button[data-qa='signup-button']");
+    this.loginAccountHeader = page.getByRole('heading', { name: 'Login to your account' });
+    this.loginEmailInput = page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address');
+    this.loginPasswordInput = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: 'Login' });
   }
 }
