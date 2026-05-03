060bd199ca80c82e8d00ab503f670ed388cadb81
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 18:01:00 +0200

    Update automationExcercises.spec.ts

--- tests/automationExcercises.spec.ts
+++ tests/automationExcercises.spec.ts
@@ -1,76 +1,62 @@
 import { test, expect } from "../fixtures/automationExcercises.fixture";
+import { RegistrationFlow } from "../flow/registration.flow";
 import { accountInformationFormUserData } from "../data/registeredUserData.data";
-import { generateNewUserData } from "../utils/generateNewUserData";
 
 test.describe("Automation excercises test cases", () => {
   test.beforeEach(async ({ page }) => {
-    
     await page.goto("");
     await page.waitForLoadState("domcontentloaded");
   });
 
-  test("TC01 - Register and delete new user", async ({
-    homePage,
-    registerSignupPage,
-    accountInformationPage,
-    createdAccountPage,
-    deletedAccountPage,
-  }) => {
+  test("TC01 - Register and delete new user", async ({ page }) => {
     //generate new user data for registration
-    const userData = generateNewUserData();
-    //verification of home page loading and accepting cookies
-    await homePage.verificationOfHomePageLoading();
-    //begin of the registration proccess
-    await homePage.loginSignupButton.click();
-    //verify of registration page loading and filling the registration form
-    await registerSignupPage.verificationOfRegisterSignupPageLoadingAndFillingForm(userData.name, userData.email);
-    //verification of account information page loading and filling the account information form
-    await accountInformationPage.verificationOfAccountInformationPageLoadingAndFillingForm();
-    //verification of account creation
-    await createdAccountPage.verificationOfCreatedAccountPageLoadingAndFinishRegistration();
-    await homePage.verificationOfCreatedAccount(userData.name);
+    const registrationFlow = new RegistrationFlow(page);
+    //registration of new user and verification of registration
+    await registrationFlow.registerNewUser();
     //deletion of created account and verification of deletion
-    await homePage.deleteCreatedAccount();
-    await deletedAccountPage.deleteAccountProccessAndVerification();
-    await homePage.verificationOfDeletedAccount();
+    await registrationFlow.deleteCreatedUser();
   });
 
-  test("TC02 - Login with created user data", async ({
-    homePage,
-    registerSignupPage,
-    accountInformationPage,
-    createdAccountPage,
-    deletedAccountPage,
-  }) => {
+  test("TC02 - Login with created user data", async ({ page }) => {
     //generate new user data for registration
-    const userData = generateNewUserData();
-    //verification of home page loading and accepting cookies
-    await homePage.verificationOfHomePageLoading();
-    //begin of the registration proccess
-    await homePage.loginSignupButton.click();
-    //verify of registration page loading and filling the registration form
-    await registerSignupPage.verificationOfRegisterSignupPageLoadingAndFillingForm(userData.name, userData.email);
-    //verification of account information page loading and filling the account information form
-    await accountInformationPage.verificationOfAccountInformationPageLoadingAndFillingForm();
-    //verification of account creation
-    await createdAccountPage.verificationOfCreatedAccountPageLoadingAndFinishRegistration();
-    await homePage.verificationOfCreatedAccount(userData.name);
-    //logout and login with created user data
-    await homePage.logoutButton.click();
-    await registerSignupPage.loginWithUserData(userData.email, accountInformationFormUserData.password);
-    await homePage.verificationOfCreatedAccount(userData.name);
+    const registrationFlow = new RegistrationFlow(page);
+    //registration of new user and verification of registration
+    await registrationFlow.registerNewUser();
+    //logout from created account and verification of logout
+    await registrationFlow.logoutFromCreatedUser();
+    //login with created user data and verification of login
+    await registrationFlow.loginWithCreatedUserData();
+    //deletion of created account and verification of deletion
+    await registrationFlow.deleteCreatedUser();
   });
+  //generate new user data for registration
 
-  test("TC03 - Login with incorrect email and password", async ({ homePage, registerSignupPage }) => {
+  test("TC03 - Login with incorrect email and password", async ({ page }) => {
+    const registrationFlow = new RegistrationFlow(page);
     //verification of home page loading and accepting cookies
-    await homePage.verificationOfHomePageLoading();
-    //begin of the registration proccess
-    await homePage.loginSignupButton.click();
-    //verify of registration page loading and filling the registration form
-    await registerSignupPage.loginWithUserData("incorrect@example.com", "incorrectpassword");
+    await registrationFlow.homePage.verificationOfHomePageLoading();
+    //begin of the login proccess
+    await registrationFlow.homePage.loginSignupButton.click();
+    //verify of registration/signup page loading and filling the login form with incorrect email and password
+    await registrationFlow.registerSignupPage.loginWithUserData(
+      "incorrect@example.com",
+      "incorrectpassword",
+    );
     //verification of error message for incorrect email and password
-    await registerSignupPage.displayErrorMessageAfterLoginWithIncorrectData();
+    await registrationFlow.registerSignupPage.displayErrorMessageAfterLoginWithIncorrectData();
   });
 
-  
+  test("TC04 - Logout user", async ({ page }) => {
+    const registrationFlow = new RegistrationFlow(page);
+    //registration of new user and verification of registration
+    await registrationFlow.registerNewUser();
+    //logout from created account and verification of logout
+    await registrationFlow.logoutFromCreatedUser();
+    //login with created user data and verification of login
+    await registrationFlow.loginWithCreatedUserData();
+    //repeat of logout from created account and verification of logout
+    await registrationFlow.logoutFromCreatedUser();
+    await expect(registrationFlow.homePage.loggedInAsText).toBeHidden();
+    await expect(registrationFlow.registerSignupPage.loginAccountHeader).toBeVisible();
+  });
 });
