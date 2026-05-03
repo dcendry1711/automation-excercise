6939d70d43a44a427ab10b838276181f5fff2904
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 11:58:35 +0200

    Update registerSignup.page.ts

--- pages/registerSignup.page.ts
+++ pages/registerSignup.page.ts
@@ -1,16 +1,22 @@
-import {Locator, Page} from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export class RegisterSignupPage {
+  signupForm: Locator;
+  signupNameInput: Locator;
+  signupEmailInput: Locator;
+  signupButton: Locator;
 
-    signupForm: Locator;
-    signupNameInput: Locator;
-    signupEmailInput: Locator
-    signupButton: Locator;
+  async verificationOfRegisterSignupPageLoadingAndFillingForm() {
+    await expect(this.signupForm).toContainText("New User Signup!");
+    await this.signupNameInput.fill("Daniel");
+    await this.signupEmailInput.fill("dan990.cendry@gmail.com");
+    await this.signupButton.click();
+  }
 
-    constructor(private page: Page) {
-        this.signupForm = page.locator(".signup-form");
-        this.signupNameInput = page.locator("input[data-qa='signup-name']");
-        this.signupEmailInput = page.locator("input[data-qa='signup-email']");
-        this.signupButton = page.locator("button[data-qa='signup-button']");
-    }
+  constructor(private page: Page) {
+    this.signupForm = page.locator(".signup-form");
+    this.signupNameInput = page.locator("input[data-qa='signup-name']");
+    this.signupEmailInput = page.locator("input[data-qa='signup-email']");
+    this.signupButton = page.locator("button[data-qa='signup-button']");
+  }
 }
