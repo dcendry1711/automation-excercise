8351a4500a86b60dc2682427fe46fb24eab94ced
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 11:58:31 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,19 +1,47 @@
-import {Locator, Page} from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
+import { registerSignupFormUserData } from "../data/registeredUserData.data";
 
 export class HomePage {
+  consentAcceptButton: Locator;
+  leftSidebar: Locator;
+  loginSignupButton: Locator;
+  loggedInAsText: Locator;
+  deleteAccountButton: Locator;
 
-    consentAcceptButton: Locator;
-    leftSidebar: Locator;
-    loginSignupButton: Locator;
-    loggedInAsText: Locator;
-    deleteAccountButton: Locator;
+  async verificationOfHomePageLoading() {
+    await this.consentAcceptButton.click();
+    await expect(this.leftSidebar).toBeVisible();
+    await expect(this.loginSignupButton).toBeVisible();
+  }
 
-    constructor(private page: Page) {
-        this.consentAcceptButton = page.locator(".fc-button.fc-cta-consent.fc-primary-button");
-        this.leftSidebar = page.locator(".left-sidebar");
-        this.loginSignupButton = page.locator("a", { hasText: "Signup / Login" });
-        this.loggedInAsText = page.getByText('Logged in as Daniel')
-        this.deleteAccountButton = page.getByRole('link', { name: ' Delete Account' });
-    }
+  async verificationOfCreatedAccount() {
+    await expect(this.loggedInAsText).toBeVisible();
+    await expect(this.loggedInAsText).toContainText(
+      `Logged in as ${registerSignupFormUserData.name}`,
+    );
+  }
+
+  async deleteCreatedAccount() {
+    await this.deleteAccountButton.click();
+  }
+
+  async verificationOfDeletedAccount() {
+    await expect(this.loggedInAsText).not.toBeVisible();
+    await expect(this.loginSignupButton).toBeVisible();
+    await expect(this.leftSidebar).toBeVisible();
+  }
+
+  constructor(private page: Page) {
+    this.consentAcceptButton = page.locator(
+      ".fc-button.fc-cta-consent.fc-primary-button",
+    );
+    this.leftSidebar = page.locator(".left-sidebar");
+    this.loginSignupButton = page.locator("a", { hasText: "Signup / Login" });
+    this.loggedInAsText = page.getByText(
+      `Logged in as ${registerSignupFormUserData.name}`,
+    );
+    this.deleteAccountButton = page.getByRole("link", {
+      name: " Delete Account",
+    });
+  }
 }
-
