76b849aa4efc6e9732536f63fb5e221e43d6019f
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 11:58:10 +0200

    Create registeredUserData.data.ts

---
+++ data/registeredUserData.data.ts
@@ -0,0 +1,24 @@
+export const registerSignupFormUserData = {
+  name: "Daniel",
+  email: "dan990.cendry@gmail.com",
+};
+
+export const accountInformationFormUserData = {
+  title: "Mr",
+  password: "123456",
+  dayOfBirth: "10",
+  monthOfBirth: "5",
+  yearOfBirth: "1990",
+  newsletterCheckbox: true,
+  specialOffersCheckbox: true,
+  firstName: "Daniel",
+  lastName: "Cendry",
+  company: "Test Company",
+  address: "Test Street 123",
+  address2: "Test Street 456",
+  country: "United States",
+  state: "Test State",
+  city: "Test City",
+  zipcode: "12345",
+  mobileNumber: "+1234567890",
+};
