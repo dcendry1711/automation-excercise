87e424ff30114886fae2015a4fb2e05efc0cb8f5
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 16:52:10 +0200

    Create automationExcercises.spec.ts

---
+++ tests/automationExcercises.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from "../fixtures/automationExcercises.fixture";
+import { accountInformationFormUserData } from "../data/registeredUserData.data";
+import { generateNewUserData } from "../utils/generateNewUserData";
+
+test.describe("Automation excercises test cases", () => {
+  test.beforeEach(async ({ page }) => {
+    
+    await page.goto("");
+    await page.waitForLoadState("domcontentloaded");
+  });
+
+  const userData = generateNewUserData();
+
+  test("TC01 - Register and delete new user", async ({
+    homePage,
+    registerSignupPage,
+    accountInformationPage,
+    createdAccountPage,
+    deletedAccountPage,
+  }) => {
+    //verification of home page loading and accepting cookies
+    await homePage.verificationOfHomePageLoading();
+    //begin of the registration proccess
+    await homePage.loginSignupButton.click();
+    //verify of registration page loading and filling the registration form
+    await registerSignupPage.verificationOfRegisterSignupPageLoadingAndFillingForm(userData.name, userData.email);
+    //verification of account information page loading and filling the account information form
+    await accountInformationPage.verificationOfAccountInformationPageLoadingAndFillingForm();
+    //verification of account creation
+    await createdAccountPage.verificationOfCreatedAccountPageLoadingAndFinishRegistration();
+    await homePage.verificationOfCreatedAccount(userData.name);
+    //deletion of created account and verification of deletion
+    await homePage.deleteCreatedAccount();
+    await deletedAccountPage.deleteAccountProccessAndVerification();
+    await homePage.verificationOfDeletedAccount();
+  });
+
+  test("TC02 - Login with created user data", async ({
+    homePage,
+    registerSignupPage,
+    accountInformationPage,
+    createdAccountPage,
+    deletedAccountPage,
+  }) => {
+    await homePage.verificationOfHomePageLoading();
+    await homePage.loginSignupButton.click();
+    await registerSignupPage.verificationOfRegisterSignupPageLoadingAndFillingForm(userData.name, userData.email);
+    await accountInformationPage.verificationOfAccountInformationPageLoadingAndFillingForm();
+    await createdAccountPage.verificationOfCreatedAccountPageLoadingAndFinishRegistration();
+    await homePage.verificationOfCreatedAccount(userData.name);
+    await homePage.logoutButton.click();
+    await registerSignupPage.loginWithCreatedUserData(userData.email, accountInformationFormUserData.password);
+    await homePage.verificationOfCreatedAccount(userData.name);
+    await homePage.deleteCreatedAccount();
+    await deletedAccountPage.deleteAccountProccessAndVerification();
+    await homePage.verificationOfDeletedAccount();
+  });
+});
