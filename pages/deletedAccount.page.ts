5b9fe5c4c4ee2b4f71f73c4a82cbe15bb3bbda60
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 11:58:28 +0200

    Update deletedAccount.page.ts

--- pages/deletedAccount.page.ts
+++ pages/deletedAccount.page.ts
@@ -1,12 +1,18 @@
-import {Locator, Page} from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export class DeletedAccountPage {
+  deletedAccountMessage: Locator;
+  finishDeletingAccountButton: Locator;
 
-    deletedAccountMessage: Locator;
-    finishDeletingAccountButton: Locator;
+  async deleteAccountProccessAndVerification() {
+    await expect(this.deletedAccountMessage).toContainText("Account Deleted!");
+    await this.finishDeletingAccountButton.click();
+  }
 
-    constructor(private page: Page) {
-        this.deletedAccountMessage = page.getByText('Account Deleted!')
-        this.finishDeletingAccountButton = page.locator('div').filter({ hasText: /^Continue$/ });
-    }
+  constructor(private page: Page) {
+    this.deletedAccountMessage = page.getByText("Account Deleted!");
+    this.finishDeletingAccountButton = page
+      .locator("div")
+      .filter({ hasText: /^Continue$/ });
+  }
 }
