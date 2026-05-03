e2aef1f316b0fd48c1397e5b6236006c41e476a5
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 17:05:54 +0200

    Update registerSignup.page.ts

--- pages/registerSignup.page.ts
+++ pages/registerSignup.page.ts
@@ -10,6 +10,7 @@ export class RegisterSignupPage {
   loginEmailInput: Locator;
   loginPasswordInput: Locator;
   loginButton: Locator;
+  errorLoginMessage: Locator;
 
   async verificationOfRegisterSignupPageLoadingAndFillingForm(name: string, email: string) {
     await expect(this.signupForm).toContainText("New User Signup!");
@@ -18,11 +19,16 @@ export class RegisterSignupPage {
     await this.signupButton.click();
   }
 
-  async loginWithCreatedUserData(email: string, password: string) {
+  async loginWithUserData(email: string, password: string) {
     await expect(this.loginAccountHeader).toContainText("Login to your account");
     await this.loginEmailInput.fill(email);
     await this.loginPasswordInput.fill(password);
     await this.loginButton.click();
+  }
+
+  async displayErrorMessageAfterLoginWithIncorrectData() {
+    await expect(this.errorLoginMessage).toBeVisible();
+    await expect(this.errorLoginMessage).toContainText("Your email or password is incorrect!");
   }
 
   constructor(private page: Page) {
@@ -34,5 +40,6 @@ export class RegisterSignupPage {
     this.loginEmailInput = page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address');
     this.loginPasswordInput = page.getByRole('textbox', { name: 'Password' });
     this.loginButton = page.getByRole('button', { name: 'Login' });
+    this.errorLoginMessage = page.getByText('Your email or password is');
   }
 }
