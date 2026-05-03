3a6e5d18f4a66182f21b6e7eaf2d6a082118513e
dcendry1711 <daniel.cendry@gmail.com>
2026-05-03 11:58:25 +0200

    Update createdAcc.page.ts

--- pages/createdAcc.page.ts
+++ pages/createdAcc.page.ts
@@ -1,12 +1,18 @@
-import { Locator, Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 
 export class CreatedAccountPage {
+  createdAccountMessage: Locator;
+  finishCreatingAccountButton: Locator;
 
-    createdAccountMessage: Locator;
-    finishCreatingAccountButton: Locator;
+  async verificationOfCreatedAccountPageLoadingAndFinishRegistration() {
+    await expect(this.createdAccountMessage).toContainText("Account Created!");
+    await this.finishCreatingAccountButton.click();
+  }
 
-    constructor(private page: Page) {
-        this.createdAccountMessage = page.getByText('Account Created!')
-        this.finishCreatingAccountButton = page.getByRole('link', { name: 'Continue' });
-    }
+  constructor(private page: Page) {
+    this.createdAccountMessage = page.getByText("Account Created!");
+    this.finishCreatingAccountButton = page.getByRole("link", {
+      name: "Continue",
+    });
+  }
 }
