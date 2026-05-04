d0f85b90a874131b4a1765929f79991c0e84e8fa
dcendry1711 <daniel.cendry@gmail.com>
2026-05-04 13:58:20 +0200

    Create contactUsForm.data.ts

---
+++ data/contactUsForm.data.ts
@@ -0,0 +1,7 @@
+export const contactUsFormData = {
+    name: "Daniel Cendry",
+    email: "daniel.cendry@gmail.com",
+    subject: "Test subject",
+    message: "This is a test message.",
+    filePath: "fileToUpload.txt"
+}
