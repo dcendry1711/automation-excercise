cc82fd30a3a31b787996f7836c033cb053a2bd13
dcendry1711 <daniel.cendry@gmail.com>
2026-05-05 19:10:59 +0200

    Update home.page.ts

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -3,16 +3,22 @@ import { Locator, Page, expect } from "@playwright/test";
 export class HomePage {
   consentAcceptButton: Locator;
   leftSidebar: Locator;
-  loginSignupButton: Locator;
+  loginSignupMenuButton: Locator;
   loggedInAsText: Locator;
   deleteAccountButton: Locator;
-  logoutButton: Locator;
-  contactUsButton: Locator;
+  logoutMenuButton: Locator;
+  contactUsMenuButton: Locator;
+  testCasesMenuButton: Locator;
+  productsMenuButton: Locator;
+  subscriptionFooterHeader: Locator;
+  subscriptionEmailInput: Locator;
+  subscriptionSubmitButton: Locator;
+  subscriptionSuccessMessage: Locator;
 
   async verificationOfHomePageLoading() {
     await this.consentAcceptButton.click();
     await expect(this.leftSidebar).toBeVisible();
-    await expect(this.loginSignupButton).toBeVisible();
+    await expect(this.loginSignupMenuButton).toBeVisible();
   }
 
   async verificationOfCreatedAccount(name: string) {
@@ -25,12 +31,12 @@ export class HomePage {
   }
 
   async logoutFromAccount() {
-    await this.logoutButton.click();
+    await this.logoutMenuButton.click();
   }
 
   async verificationOfDeletedAccount() {
     await expect(this.loggedInAsText).not.toBeVisible();
-    await expect(this.loginSignupButton).toBeVisible();
+    await expect(this.loginSignupMenuButton).toBeVisible();
     await expect(this.leftSidebar).toBeVisible();
   }
 
@@ -39,12 +45,18 @@ export class HomePage {
       ".fc-button.fc-cta-consent.fc-primary-button",
     );
     this.leftSidebar = page.locator(".left-sidebar");
-    this.loginSignupButton = page.locator("a", { hasText: "Signup / Login" });
+    this.loginSignupMenuButton = page.locator("a", { hasText: "Signup / Login" });
     this.loggedInAsText = page.getByText(`Logged in as`);
     this.deleteAccountButton = page.getByRole("link", {
       name: " Delete Account",
     });
-    this.logoutButton = page.getByRole("link", { name: " Logout" });
-    this.contactUsButton = page.getByRole("link", { name: " Contact us" });
+    this.logoutMenuButton = page.getByRole("link", { name: " Logout" });
+    this.contactUsMenuButton = page.getByRole("link", { name: " Contact us" });
+    this.testCasesMenuButton = page.getByRole('link', { name: ' Test Cases' });
+    this.productsMenuButton = page.getByRole('link', { name: ' Products' });
+    this.subscriptionFooterHeader = page.getByRole('heading', { name: 'Subscription' });
+    this.subscriptionEmailInput = page.getByRole('textbox', { name: 'Your email address' });
+    this.subscriptionSubmitButton = page.locator('#subscribe');
+    this.subscriptionSuccessMessage = page.getByText('You have been successfully');
   }
 }
